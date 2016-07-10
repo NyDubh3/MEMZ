1f0e468d3d25a4d46043df2be93a4eb299bcb014
Leurak <Leurak@users.noreply.github.com>
2016-07-10 14:54:43 +0200

    Add disclaimer for destructive version
    Using this I can make the downloads open again!

--- VCProject/MEMZ/main.cpp
+++ VCProject/MEMZ/main.cpp
@@ -45,6 +45,19 @@ void main() {
 			}
 		}
 	} else {
+		// Another very ugly formatting
+		if (MessageBoxA(NULL, "The software you just executed is considered malware.\r\n\
+This malware will harm your computer and makes it unusable.\r\n\
+If you are seeing this message without knowing what you just executed, simply press No and nothing will happen.\r\n\
+If you know what this malware does and are using a safe environment to test, \
+press Yes to start it.\r\n\r\n\
+DO YOU WANT TO EXECUTE THIS MALWARE, RESULTING IN AN UNUSABLE MACHINE?", "MEMZ", MB_YESNO | MB_ICONWARNING) != IDYES ||
+MessageBoxA(NULL, "THIS IS THE LAST WARNING!\r\n\r\n\
+THE CREATOR IS NOT RESPONSIBLE FOR ANY DAMAGE MADE USING THIS MALWARE!\r\n\
+STILL EXECUTE IT?", "MEMZ", MB_YESNO | MB_ICONWARNING) != IDYES) {
+			ExitProcess(0);
+		}
+
 		wchar_t *fn = (wchar_t *)LocalAlloc(LMEM_ZEROINIT, 8192*2);
 		GetModuleFileName(NULL, fn, 8192);
 
